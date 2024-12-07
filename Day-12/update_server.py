6f2f551d5b58cec9638e0f0e30facc2bb0c5ee14
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2024-12-07 20:44:05 +0530

    Update update_server.py

--- Day-12/update_server.py
+++ Day-12/update_server.py
@@ -19,7 +19,10 @@ server_config_file = 'server.conf'
 
 # Key and new value for updating the server configuration
 key_to_update = 'MAX_CONNECTIONS'
-new_value = '600'  # New maximum connections allowed
+new_value = '800'  # New maximum connections allowed
+
+key_to_update = 'TIMEOUT'
+new_value = '40'
 
 # Update the server configuration file
 update_server_config(server_config_file, key_to_update, new_value)
